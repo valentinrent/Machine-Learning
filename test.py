c7677bd10d81c10700728a1f719858c1873435b8
Valentin <valentin.rentschler.vr@gmail.com>
2024-05-07 13:41:42 +0200

    commit

---
+++ test.py
@@ -0,0 +1,28 @@
+#testing imports
+
+import scipy
+import numpy as np
+import matplotlib
+import pandas as pd
+
+
+list = []
+
+list.append(6)
+
+
+print(list)
+
+
+# Create a 2D numpy array
+arr = np.array([[1, 2, 3]])
+
+# Print the shape of the array
+print("Shape of the array:", arr.shape)
+
+# Print the size of the second dimension
+print("Size of the second dimension:", arr.shape[1])
+
+
+
+
